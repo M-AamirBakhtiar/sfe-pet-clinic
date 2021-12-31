9fd241e8b575ceffbc20cd3274c8db00c88aa02c
aamir bakhtiar <aamirbakhtiar.ab@gmail.com>
2022-01-01 00:00:24 +0500

    Initial Project Add

---
+++ src/main/java/com/springframework/sfepetclinic/SfePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sfepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/sfepetclinic/SfePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
