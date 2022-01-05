b254bd6bf13a31ab04013b407331fc64adc572b9
aamir bakhtiar <aamirbakhtiar.ab@gmail.com>
2022-01-05 18:58:58 +0500

    Add Specialties Service Impl. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfepetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springframework.sfepetclinic.services;
+
+import com.springframework.sfepetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
