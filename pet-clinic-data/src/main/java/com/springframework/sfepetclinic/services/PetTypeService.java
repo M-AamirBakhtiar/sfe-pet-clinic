ed6fb38a442fbccd7a170be145c322542be3319c
aamir bakhtiar <aamirbakhtiar.ab@gmail.com>
2022-01-05 13:58:51 +0500

    Add PetTypeService. Closes #28.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfepetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.sfepetclinic.services;
+
+import com.springframework.sfepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
